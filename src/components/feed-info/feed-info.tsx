f57e8ae9a72e24ad2370ee2f5b2b0de0fd002a2b
Egor1dzeN <egormorozov-yandex@mail.ru>
2025-05-28 21:06:59 +0300

    feat: burger-ingredient + feed_info

--- src/components/feed-info/feed-info.tsx
+++ src/components/feed-info/feed-info.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 
 import { TOrder } from '@utils-types';
-import { FeedInfoUI } from '../ui/feed-info';
+import { FeedInfoUI } from '@ui';
+import { useSelector } from '@app-store';
+import { getOrders as getOrders_, getTotal, getTotalToday } from '@slices';
 
 const getOrders = (orders: TOrder[], status: string): number[] =>
   orders
@@ -10,9 +12,13 @@ const getOrders = (orders: TOrder[], status: string): number[] =>
     .slice(0, 20);
 
 export const FeedInfo: FC = () => {
-  /** TODO: взять переменные из стора */
-  const orders: TOrder[] = [];
-  const feed = {};
+  const tot = useSelector(getTotal);
+  const totToday = useSelector(getTotalToday);
+  const orders: TOrder[] = useSelector(getOrders_);
+  const feed = {
+    tot,
+    totToday
+  };
 
   const readyOrders = getOrders(orders, 'done');
 
